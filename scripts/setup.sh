7caa073bab712f7e0d9bb7c65462d7ac658330a7
Yibo Yan <yiboyan@usc.edu>
2025-12-15 21:31:19 +0000

    add: script for setup passowrdless localhost ssh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,3 +5,4 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SOURCE_DIR/setup_gdb.sh
 source $SOURCE_DIR/setup_mqtt.sh
 source $SOURCE_DIR/setup_ptrace.sh
+source $SOURCE_DIR/setup_local_ssh.sh
