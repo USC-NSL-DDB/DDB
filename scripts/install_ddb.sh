9a8757da0ce2b349a51e2843abf09cb4788a7b79
Yibo Yan <yiboyan@usc.edu>
2025-12-16 00:08:32 +0000

    nits

--- scripts/install_ddb.sh
+++ scripts/install_ddb.sh
@@ -2,6 +2,13 @@
 
 # This scripts is used for end users to install DDB in one click.
 
+sudo apt-get update
+sudo apt-get install -y \
+    git build-essential cmake python3 python3-pip \
+    cmake make
+
+set -e
+
 mkdir $HOME/.ddb_src
 pushd $HOME/.ddb_src
 git clone https://github.com/USC-NSL-DDB/DDB.git
