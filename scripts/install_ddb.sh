7a58366c2bf6936d80fc5cf89fbe71cd1b226607
Yibo Yan <yiboyan@usc.edu>
2026-01-26 14:02:03 +0000

    feat: auto download vscode extension

--- scripts/install_ddb.sh
+++ scripts/install_ddb.sh
@@ -5,11 +5,19 @@
 sudo apt-get update
 sudo apt-get install -y \
     git build-essential cmake python3 python3-pip \
-    cmake make gdb
+    cmake make gdb wget unzip
 
 set -e
 mkdir -p $HOME/.ddb_src
 pushd $HOME/.ddb_src
+
+# Download VSCode extension for DDB
+rm -rf *.zip || true
+rm -rf *.vsix || true
+wget https://github.com/USC-NSL-DDB/vscode-adapter/releases/download/v0.0.6-alpha/ddb-debugger-vsix.zip
+unzip ddb-debugger-vsix.zip
+rm -rf *.zip
+
 rm -rf DDB
 git clone --depth 1 --branch main https://github.com/USC-NSL-DDB/DDB.git
 pushd DDB/scripts
